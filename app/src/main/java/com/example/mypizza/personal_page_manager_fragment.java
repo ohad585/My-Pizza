ce1d0e46b08e35a1433f3b9327394240d53365bf
ohad shalom <ohad5855@gmail.com>
2022-01-24 09:49:14 +0200

    java files

---
+++ app/src/main/java/com/example/mypizza/personal_page_manager_fragment.java
@@ -0,0 +1,18 @@
+package com.example.mypizza;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class personal_page_manager_fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.personal_page_manager__fragment, container, false);
+    }
+}
