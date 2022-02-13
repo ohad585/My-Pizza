683981c46de25addb6c8a126b9556b9c72b0c144
ohad shalom <ohad5855@gmail.com>
2022-02-13 18:56:34 +0200

    review display in pizza details

--- app/src/main/java/com/example/mypizza/pizza_details_fragment.java
+++ app/src/main/java/com/example/mypizza/pizza_details_fragment.java
@@ -1,10 +1,18 @@
 package com.example.mypizza;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,17 +22,32 @@ import android.widget.TextView;
 
 import com.example.mypizza.Model.Model;
 import com.example.mypizza.Model.Pizza;
+import com.example.mypizza.Model.Review;
 import com.squareup.picasso.Picasso;
+
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class pizza_details_fragment extends Fragment {
     View view;
     Pizza p;
+    allReviewViewModel viewModel;
+    MyAdapter adapter;
+    SwipeRefreshLayout swipeRefresh;
 
+    List<Review> pizzaReviewList = new LinkedList<>();
     TextView price;
     TextView tops;
     ImageView img;
     Button writeReviewBtn;
     View progBar;
+
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        viewModel = new ViewModelProvider(this).get(allReviewViewModel.class);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -40,6 +63,12 @@ public class pizza_details_fragment extends Fragment {
         img = view.findViewById(R.id.pizza_details_img);
         progBar = view.findViewById(R.id.pizza_details_progBar);
         writeReviewBtn = view.findViewById(R.id.pizza_details_review_btn);
+        swipeRefresh = view.findViewById(R.id.pizza_details_swipe_refresh);
+        RecyclerView list = view.findViewById(R.id.pizza_details_reviews_rv);
+        list.setHasFixedSize(true);
+        list.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new MyAdapter();
+        list.setAdapter(adapter);
         writeReviewBtn.setClickable(false);
         writeReviewBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,11 +77,40 @@ public class pizza_details_fragment extends Fragment {
                 Navigation.findNavController(view).navigate(action);
             }
         });
+        if(viewModel.getData()==null){refreshData();};
+        viewModel.getData().observe(getViewLifecycleOwner(), new Observer<List<Review>>() {
+            @Override
+            public void onChanged(List<Review> reviews) {
+                updatePizzaReviewList();
+            }
+
+        });
+
+        swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Model.instance.reloadReviewsList();
+            }
+        });
+
+        adapter.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
+                //progBar.setVisibility(View.VISIBLE);
+                Review re = pizzaReviewList.get(position);
+                Log.d("TAG", "review is clicked: "+re.getReview());
+            }
+        });
+
+        swipeRefresh.setRefreshing(Model.instance.getReviewListLoadingState().getValue()== Model.LoadingState.loading);
+        Model.instance.getReviewListLoadingState().observe(getViewLifecycleOwner(),loadingState ->
+                swipeRefresh.setRefreshing(loadingState == Model.LoadingState.loading));
 
         progBar.setVisibility(View.VISIBLE);
         return view;
     }
     void update(){
+        Log.d("TAG", "update: ");
         price.setText(p.getPrice());
         tops.setText(p.getDescription());
         String url = p.getImgUrl();
@@ -65,5 +123,92 @@ public class pizza_details_fragment extends Fragment {
         }
         progBar.setVisibility(View.INVISIBLE);
         writeReviewBtn.setClickable(true);
+        updatePizzaReviewList();
+    }
+
+    void updatePizzaReviewList(){
+        if(p==null) {
+            Log.d("TAG", "updatePizzaReviewList: ");
+            return;
+        }
+        pizzaReviewList=new LinkedList<>();
+        for(Review r:viewModel.getData().getValue()){
+            if(r.getPizzaID().matches(p.getDescription())){
+                pizzaReviewList.add(r);
+            }
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    private void refreshData() {
+        updatePizzaReviewList();
+    }
+
+    interface OnItemClickListener{
+        void onItemClick(int position, View v);
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView reviewText;
+        TextView writerEmail;
+        ImageView pizzaImg;
+
+        public MyViewHolder(@NonNull View itemView, OnItemClickListener listener) {
+            super(itemView);
+            reviewText = itemView.findViewById(R.id.review_line_show_review_tv);
+            writerEmail = itemView.findViewById(R.id.review_line_show_emailAdd_tv);
+            pizzaImg = itemView.findViewById(R.id.review_line_show_img);
+            ImageView editImg = itemView.findViewById(R.id.review_line_show_edit_img);
+            editImg.setVisibility(View.INVISIBLE);
+            ImageView binImg = itemView.findViewById(R.id.review_line_show_bin_img);
+            binImg.setVisibility(View.INVISIBLE);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    if (listener != null) {
+                        listener.onItemClick(pos,v);
+                    }
+                }
+            });
+        }
+
+        public void bind(Review r) {
+            reviewText.setText(r.getReview());
+            writerEmail.setText(r.getWriterEmail());
+
+        }
+    }
+
+
+
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+        OnItemClickListener listener;
+        public void setOnItemClickListener(OnItemClickListener listener){
+            this.listener = listener;
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = getLayoutInflater().inflate(R.layout.review_line_show, parent, false);
+            MyViewHolder holder = new MyViewHolder(view,listener);
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+            Review review = pizzaReviewList.get(position);
+            holder.bind(review);
+        }
+
+
+        @Override
+        public int getItemCount() {
+            if (pizzaReviewList == null) return 0;
+            return pizzaReviewList.size();
+        }
+
+
     }
 }
